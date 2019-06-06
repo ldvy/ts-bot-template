16ed3a1b225e541e54f7b83b5cecd16b07a45b1c
vilkup <pride2808@gmail.com>
2019-06-06 11:56:47 +0300

    asdasf

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-```node
-//node code 
-console.log('I love TS!')
+# Superb template for building Telegram Bots
+## It's based on a couple of pretty cool technologies:
+* [Typescript](https://www.typescriptlang.org) ❤
+* [Telegraf](https://telegraf.js.org)📡
+* [MongoDB](https://www.mongodb.com/what-is-mongodb) 🔮
+* [Mongoose](https://github.com/Automattic/mongoose/) 🐿️
+
+`src` folder has the next structure:  
+```bash
+├───controllers
+├───handlers
+├───helpers
+├───init
+├───middlewares
+├───models
+└───scenes
 ```
+### So, let's learn how to build bots with TS-Bot-Template!
--- src/init/bot.ts
+++ src/init/bot.ts
@@ -2,18 +2,15 @@ import Telegraf from 'telegraf'
 import Logger from './logger'
 import config from '../config.json'
 
-const Stage = require('telegraf/stage')
-const WizardScene = require('telegraf/scenes/wizard')
-
 export default class Bot { 
     private static token: string
 
-    public static configure() {
+    public static async configure() {
         // Проверка окружения и смена токена
         process.env.NODE_ENV === 'production' ? this.token = config.prod.token : this.token = config.dev.token
 
         const bot = new Telegraf(this.token)    // Создание обьекта
-        bot.launch()                            // Запуск
+        await bot.launch()                      // Запуск
 
         Logger.trace('>>> Бот сконфигурирован')
         return bot

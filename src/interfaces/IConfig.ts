dc248efccb2f06bac0471cb6bd6b7fa6bad27121
Vilen Kuprienko <vilkup@MacBook-Pro-Vilen.local>
2019-06-19 09:29:37 +0300

    added interface for config

---
+++ src/interfaces/IConfig.ts
@@ -0,0 +1,12 @@
+export default interface IConfig {
+    dev: {
+        token: string,
+        dbUrl: string,
+        port : number
+    },
+    prod: {
+        token: string,
+        dbUrl: string,
+        port : number
+    }
+}

4a823ad17afadb0071b810d161e2448447dd0827
Vilen Kuprienko <pride2808@gmail.com>
2019-08-07 10:07:43 +0300

    Styled

--- src/middlewares/addUsers.ts
+++ src/middlewares/addUsers.ts
@@ -7,29 +7,29 @@ import User from '../models/user'
  */
 
 export default async (ctx: api.ContextMessageUpdate, next: Function) => {
-    // Получаем данные о пользователе из контекста
-    let chatId = ctx.from.id
-    let username = ctx.from.username
-    let name = ctx.from.first_name
-    
-    // Составляем имя в зависимости от наличия фамилии
-    if (ctx.from.last_name !== undefined) name = `${ctx.from.first_name} ${ctx.from.last_name}`
-    
-    // Добавляем админа @vilkup и @kemarskiy
-    let isAdmin = [300922262, 461738219].includes(chatId)
-    
-    let insertDoc: any = {
-        chatId: chatId,
-        name: name,
-        isAdmin: isAdmin
-    }
-    
-    if (username) insertDoc.username = username
-    
-    await User.findOneAndUpdate({ chatId: chatId },
-      insertDoc,
-      { upsert: true, new: true }
-    )
-    
-    await next()
+  // Получаем данные о пользователе из контекста
+  let chatId = ctx.from.id
+  let username = ctx.from.username
+  let name = ctx.from.first_name
+  
+  // Составляем имя в зависимости от наличия фамилии
+  if (ctx.from.last_name !== undefined) name = `${ctx.from.first_name} ${ctx.from.last_name}`
+  
+  // Добавляем админа @vilkup и @kemarskiy
+  let isAdmin = [300922262, 461738219].includes(chatId)
+  
+  let insertDoc: any = {
+    chatId: chatId,
+    name: name,
+    isAdmin: isAdmin
+  }
+  
+  if (username) insertDoc.username = username
+  
+  await User.findOneAndUpdate({ chatId: chatId },
+    insertDoc,
+    { upsert: true, new: true }
+  )
+  
+  await next()
 }

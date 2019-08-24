7bcdacd16adc1c658f1817bd51b674986dfae30f
Vilen Kuprienko <pride2808@gmail.com>
2019-08-24 13:38:04 +0300

    Sessions is stored in MongoDB

--- src/init/middlewares.ts
+++ src/init/middlewares.ts
@@ -1,16 +1,18 @@
 import * as api from 'telegraf'
+import config from '../config'
 import addUsers from '../middlewares/addUsers'
 import chatLogging from '../middlewares/chatLogging'
 import Logger from './logger'
 
-const session = require('telegraf/session')
+const { TelegrafMongoSession } = require('telegraf-session-mongodb')
 
 export default class Middlewares {
   public static init(bot: api.Telegraf<api.ContextMessageUpdate>): void {
     try {
       bot.use(addUsers)       // прослойка добавления пользователя в базу
       bot.use(chatLogging)    // прослойка логирования переписки
-      bot.use(session())      // прослойка для использования сессий
+  
+      TelegrafMongoSession.setup(bot, config.dbUrl)
       
       Logger.trace('>>> Прослойки инициализированы')
     }
